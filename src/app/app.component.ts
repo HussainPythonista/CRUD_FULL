12f56c5c02ca0c65e86713f818467fa6aee41296
Mohamed Kalanthar Hussain <60617460+HussainPythonista@users.noreply.github.com>
2023-07-30 17:16:48 +0530

    Initial Project setup in angular, for full fledged

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { AuthService } from './auth.service'; // Update the path to the AuthService
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+}
---
+++ src/app/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private isLoggedInSubject = new BehaviorSubject<boolean>(false);
+  isLoggedIn$ = this.isLoggedInSubject.asObservable();
+
+  constructor() {}
+
+  updateLoggedInStatus(value: boolean) {
+    this.isLoggedInSubject.next(value);
+  }
+}
